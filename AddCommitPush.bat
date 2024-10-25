8832cce3169ebffee841111483107e187e0d75a6
l00188515 <l00188515@atu.ie>
2024-10-25 16:25:40 +0100

    $CommitMessage

---
+++ AddCommitPush.bat
@@ -0,0 +1,28 @@
+:: Batch file to Add Commit Push to GITHUB Repo
+:: By: Aisling
+:: Initial File: 25OCT24
+:: Script: AddCommitPush.bat
+
+@echo off
+cls
+title AddCommitPush
+
+git status
+
+echo '**************************************************'
+echo "Performing an add for all files in this directory"
+git add .
+git status
+
+echo '**************************************************'
+echo 'Enter the commit message:'
+read CommitMessage
+git commit -m "$CommitMessage"
+git status
+
+echo '**************************************************'
+echo 'Pushing to GITHUB repository'
+git push -u origin master
+echo '**************************************************'
+
+echo 'Done!'
